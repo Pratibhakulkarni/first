e7ca3b275d4653e1ea00718b97be7d614c7fb380
unknown <pratibhakulkarni121@gmail.com>
2022-08-26 12:57:55 +0530

    added dom.js

--- dom.js
+++ dom.js
@@ -10,5 +10,7 @@ items[0].style.color="green";
 
 //You need to git add my_project to stage your new folder.
  //Then git add my_project/* to stage its contents.
- //gitgit push -u origin main
- //git add Then commit what you've staged using git commit and finally push your changes back to the source using git push origin master
+ //git add my_cool_folder
+//git commit -m "I added my_cool_folder"
+//git push
+ //gitgit push -u origin mai //git add Then commit what you've staged using git commit and finally push your changes back to the source using git push origin master
