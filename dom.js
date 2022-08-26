0b96d7300fceb7dc7359f4c15a54f0a75e1bbd59
unknown <pratibhakulkarni121@gmail.com>
2022-08-26 12:50:53 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,14 @@
+var headerTitle=(document.getElementById('header-title'));
+console.log(headerTitle);
+headerTitle.style.border="solid 5px black"
+
+    
+var items=document.getElementsByClassName("title");
+console.log(items);
+items[0].style.fontWeight="bold";
+items[0].style.color="green";
+
+//You need to git add my_project to stage your new folder.
+ //Then git add my_project/* to stage its contents.
+ //gitgit push -u origin main
+ //git add Then commit what you've staged using git commit and finally push your changes back to the source using git push origin master
