35aa39f9ffb3c82d4c64402b870303e08471a179
unknown <pratibhakulkarni121@gmail.com>
2022-08-26 14:30:24 +0530

    queryselectorandall

--- dom.js
+++ dom.js
@@ -1,5 +1,5 @@
 
-    //to change bgcolor of 3rd element to green
+    /*//to change bgcolor of 3rd element to green
 var li=document.getElementsByTagName("li");
 console.log(li);
 for(var i=0;i<li.length;i++)
@@ -11,4 +11,21 @@ for(var i=0;i<li.length;i++)
 var liv=document.getElementsByClassName("kk");
 console.log(liv);
 liv[0].style.color="white";
-liv[0].style.fontWeight="bold";
+liv[0].style.fontWeight="bold";*/
+
+//Queryselector
+
+var sec =document.querySelector(".list-group-item:nth-child(2)");
+sec.style.backgroundColor="green";
+var third =document.querySelector(".list-group-item:nth-child(3)");
+third.style.visibility="hidden";
+
+//Queryselectorall
+
+var second =document.querySelectorAll('.list-group-item');
+second[1].style.color="green"
+var odd =document.querySelectorAll('li:nth-child(odd)');
+    for(var i=0;i<odd.length;i++)
+    {
+odd[i].style.backgroundColor="green"
+    }
